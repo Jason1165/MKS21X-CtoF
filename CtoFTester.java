6d103063151453f48f1051dcaf6c3e1ce4b9e1c8
Jason Lin <jlin31@stuy.edu>
2021-09-23 20:12:17 -0400

    Added a few more test cases.

--- CtoFTester.java
+++ CtoFTester.java
@@ -8,10 +8,14 @@ This is since 5/9 of something requires digits after the decimal point.
 public class CtoFTester{
   public static void main(String[] args) {
     // statements to test
+    System.out.println("Celsius to Fahrenheit Test Cases");
     System.out.println(celsiusToFahrenheit(0.0));
-    System.out.println(celsiusToFahrenheit(-40));
+    System.out.println(celsiusToFahrenheit(100));
+    System.out.println(celsiusToFahrenheit(-40)); // should be -40
+    System.out.println("Fahrenheit to Celsius Test Cases");
     System.out.println(fahrenheitToCelcius(32));
-    System.out.println(fahrenheitToCelcius(-40));
+    System.out.println(fahrenheitToCelcius(212));
+    System.out.println(fahrenheitToCelcius(-40)); // should be -40
   }
 
   public static double celsiusToFahrenheit(double c) {
